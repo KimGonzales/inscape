85b38536f4740f2d1fdd3af3770ae1899ccfca06
Kim Gonzales <Kimgo589@gmail.com>
2018-06-21 01:37:45 -0400

    add user associations to seed data

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_06_21_051333) do
+ActiveRecord::Schema.define(version: 2018_06_21_052507) do
 
   create_table "photos", force: :cascade do |t|
     t.string "title"
     t.text "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
   end
 
   create_table "users", force: :cascade do |t|
