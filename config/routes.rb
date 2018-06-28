0559996a0458fff5004e640aed8136043beac1fa
Kim Gonzales <Kimgo589@gmail.com>
2018-06-28 14:53:00 -0400

    add spec.md file

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
+  
   devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
-  resources :photos
+
+  resources :photos do 
+    resources :comments
+  end
+
   root 'photos#index'
   
   resources :profiles, only: [:show, :edit, :update] do
