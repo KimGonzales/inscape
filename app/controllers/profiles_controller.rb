d2d7d64190bf7e3943be2d5b20a680846db7e7a7
Kim Gonzales <Kimgo589@gmail.com>
2018-07-03 18:44:57 -0400

    convert indentation to spaces

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,30 +1,30 @@
 class ProfilesController < ApplicationController
-	before_action :set_profile, only: [:show, :edit, :update]
+  before_action :set_profile, only: [:show, :edit, :update]
 
-	def show
-		@photos = @profile.user.photos.order('created_at DESC')
-	end
+  def show
+    @photos = @profile.user.photos.order('created_at DESC')
+  end
 
-	def edit
-		@photos = @profile.photos
-	end
+  def edit
+    @photos = @profile.photos
+  end
 
-	def update
-		if @profile.update(profile_params)
-			redirect_to @profile
-		else
-			render :edit
-		end
-	end
+  def update
+    if @profile.update(profile_params)
+      redirect_to @profile
+    else
+      render :edit
+    end
+  end
 
-	private
-	
-		def set_profile
-			@profile = Profile.find(params[:id])
-		end
+  private
+  
+    def set_profile
+      @profile = Profile.find(params[:id])
+    end
 
-		def profile_params
-			params.require(:profile).permit(:bio, photos_attributes:[:id, :featured_status])
-		end
+    def profile_params
+      params.require(:profile).permit(:bio, photos_attributes:[:id, :featured_status])
+    end
 
 end 
