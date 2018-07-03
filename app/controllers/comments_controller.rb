4860b750c70c3474e0af06cd648c96fd57aef3a9
Kim Gonzales <Kimgo589@gmail.com>
2018-07-03 14:37:24 -0400

    make form partial and comments partial for comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,26 +1,22 @@
 class CommentsController < ApplicationController
-  before_action :set_comment, only: [:edit, :update, :destory]
-
-  def index
-  end
-  
-  def new
-  end
 
   def create
+    @photo = Photo.find(params[:photo_id])
+    @comment = @photo.comments.build(comment_params)
+    @comment.user_id = current_user.id
+    @comment.save!
+    redirect_to photo_path(@photo)
   end
-
-  def edit
-  end
-
-  def update
-  end
-
+  
   def destroy
+    @photo = Photo.find(params[:photo_id])
+    @comment = @photo.comments.find(params[:id])
+    @comment.destroy
+    redirect_to photo_path(@photo)
   end
 
   private
-    def set_comment
-      @comment = Comment.find_by(id: params[:id])
+    def comment_params
+      params.require(:comment).permit(:content)
     end
 end
