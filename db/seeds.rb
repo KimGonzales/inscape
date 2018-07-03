b5efa319bd19db158e4c2574a200f8ce2a585775
Kim Gonzales <Kimgo589@gmail.com>
2018-07-02 22:38:57 -0400

    add user and photo seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,13 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+user = User.create!(:email => "John@example.com", :password => 'topsecret', :password_confirmation => 'topsecret')
+user2 = User.create!(:email => "Sally@example.com", :password => 'topsecret', :password_confirmation => 'topsecret')
+
+img = File.open(File.join(Rails.root, 'app/assets/images/image1.jpg'))
+img2 = File.open(File.join(Rails.root, 'app/assets/images/image2.jpg'))
+img3 = File.open(File.join(Rails.root, 'app/assets/images/image3.jpg'))
+
+user.photos.create!(:title =>"Hello World", :description => "Copper Mug", :image => img, :featured_status => 1)
+user.photos.create!(:title =>"Pickles", :description => " Letterpress distillery 90's, dreamcatcher you 
+  probably haven't heard of them taxidermy bitters yuccie pop-up mixtape echo park kitsch.", :image => img3, :featured_status => 1)
+user2.photos.create!(:title =>"my friend", :description => "Succulents kickstarter sriracha, wayfarers craft 
+  beer polaroid jean shorts shabby chic everyday carry bushwick poutine unicorn. Unicorn poke gluten-free 
+  cray church-key keffiyeh deep v. ", :image => img2, :featured_status => 1)
