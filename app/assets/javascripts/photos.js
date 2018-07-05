440521a43c9bbdf9881f2087f7cf1f50ee1effce
Kim Gonzales <Kimgo589@gmail.com>
2018-07-05 18:14:50 -0400

    masonry alignment working

--- app/assets/javascripts/photos.js
+++ app/assets/javascripts/photos.js
@@ -1,19 +1,19 @@
-// $(function(){  
-//   $('#photos').masonry({
-//     itemSelector: '.box',
-//     columnWidth: 200,
-//     isAnimated: !Modernizr.csstransitions,
-//     isFitWidth: true
-//   });
+$(function(){  
+  $('#photos').masonry({
+    itemSelector: '.box',
+    columnWidth: 200,
+    isAnimated: !Modernizr.csstransitions,
+    isFitWidth: true
+  });
 
-// });
+});
 
-// $(function(){
-//   $('#profile-photos').masonry({
-//     itemSelector: '.box',
-//     columnWidth: 100,
-//     isAnimated: !Modernizr.csstransitions,
-//     isRTL: true
-//   });
+$(function(){
+  $('#profile-photos').masonry({
+    itemSelector: '.box',
+    columnWidth: 100,
+    isAnimated: !Modernizr.csstransitions,
+    isRTL: true
+  });
 
-// });
+});
