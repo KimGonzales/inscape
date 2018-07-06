08da7fad30dd27e1fd03997c82b2137b0263cf05
Kim Gonzales <Kimgo589@gmail.com>
2018-07-06 19:26:29 -0400

    add comment edit update

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,5 +1,6 @@
 class CommentsController < ApplicationController
   before_action :set_photo
+  before_action :set_comment, only: [:edit, :update, :destroy]
 
   def index
   end
@@ -14,9 +15,19 @@ class CommentsController < ApplicationController
       render 'index'
     end
   end
+
+  def edit
+  end
+
+  def update
+    if @comment.update(comment_params)
+      redirect_to photo_comments_path(@photo)
+    else 
+      render 'edit'
+    end
+  end
   
   def destroy
-    @comment = @photo.comments.find(params[:id])
     @comment.destroy
     redirect_to photo_path(@photo)
   end
@@ -29,6 +40,10 @@ class CommentsController < ApplicationController
 
     def set_photo
       @photo = Photo.find_by(id: params[:photo_id])
+    end
+
+    def set_comment
+      @comment = @photo.comments.find_by(id: params[:id])
     end
 
 end
