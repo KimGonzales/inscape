bac8420538ad01266f964dfef914164fc5653f8e
Kim Gonzales <Kimgo589@gmail.com>
2018-07-06 14:11:44 -0400

    comment form spacing

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-  #it raises an "Exception" if there isn't sufficient protection, or if there is a CSFR-type request. The "Exception" is whatever caused the fault to begin with. This forces Rails to tell you what caused the exception
+  #it raises an "Exception" if there isn't sufficient protection, or if there is a CSFR-type request. 
+  #The "Exception" is whatever caused the fault to begin with. This forces Rails to tell you what caused the exception
   before_action :authenticate_user! 
 
   def after_sign_in_path_for(resource)
