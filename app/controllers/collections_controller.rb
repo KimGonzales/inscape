ea1eadb3ff3d540f6894e0d5ca1efecc8ccb09f5
Kim Gonzales <Kimgo589@gmail.com>
2018-07-07 12:19:22 -0400

    add delete to collections

--- app/controllers/collections_controller.rb
+++ app/controllers/collections_controller.rb
@@ -1,4 +1,10 @@
 class CollectionsController < ApplicationController
+  before_action :set_photo, only: [:new, :create, :edit, :udpate, :destroy]
+  before_action :set_collection, only: [:destroy]
+  
+  def index
+    @collections = current_user.collections
+  end
 
   def new
     @collection = Collection.new
@@ -6,7 +12,7 @@ class CollectionsController < ApplicationController
 
   def create
     @collection = current_user.collections.find_or_initialize_by(name: params[:collection][:name])
-    @collection.photos << Photo.find(params[:photo_id])
+    @collection.photos << @photo
     @collection.save
 
     if @collection.save
@@ -17,6 +23,19 @@ class CollectionsController < ApplicationController
   end
 
   def show
+    @collection = Collection.find_by(id: params[:id])
+  end
+
+  def destroy
+    @collection.photos.delete(@photo)
+  end
+
+private 
+  def set_photo 
+    @photo = Photo.find_by(id: params[:photo_id])
+  end
+
+  def set_collection
     @collection = Collection.find_by(id: params[:id])
   end
 
--- config/routes.rb
+++ config/routes.rb
@@ -18,6 +18,6 @@ Rails.application.routes.draw do
     resources :collections
   end
   
-  get 'collections/:id', to: 'collections#show', as: :collection
+  resources :collections
   
 end
