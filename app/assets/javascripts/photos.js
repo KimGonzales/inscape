46049a40360c5cc372620ced0b2effd6b4575e40
Kim Gonzales <Kimgo589@gmail.com>
2018-09-10 11:29:30 -0400

    add turbolinks load for event listener

--- app/assets/javascripts/photos.js
+++ app/assets/javascripts/photos.js
@@ -1,18 +1,49 @@
+$(document).on('turbolinks:load',
+function(){
+  attachListeners()
+})
 
-$(function(){
-    $(".js-get-photos").on("click", function(){
-      var id = $(this).data("id");
-      $.getJSON(`/profiles/${id}/photos`, function(data){
-        for (const key in data){
-          let photoid = data[key]["id"];
-          let photoTitle = data[key]["title"];
-          let photoDescription = data[key]["description"];
-          let photoImage = data[key]["description"];
-          
-          let photoDiv= $(".all-photos")
-          photoDiv.html += (`<h1>${photoTitle}</h1><p>${photoDescription}</p>`);
-          //i want to get every photo's details in my all-photos div
-        }
-      })
-    })
+function attachListeners(){
+  $(".js-get-photos").on("click", function(){
+    getPhotos();
   })
+  // $(".js-get-photos").on("click", function(){
+  //   var id = $(this).data("id");
+  //   $.getJSON(`/profiles/${id}/photos`, function(data){
+  //     for (const key in data){
+  //       let photoid = data[key]["id"];
+  //       let photoTitle = data[key]["title"];
+  //       let photoDescription = data[key]["description"];
+  //       let photoImage = data[key]["description"];
+        
+  //       console.log(photoid, photoTitle, photoDescription)
+  //       let photoDiv = $(".all-photos")
+  //       let title = document.createElement('h1');
+  //       title.innerHTML = photoTitle;
+  //       photoDiv.append(title);
+
+
+  //       //i want to get every photo's details in my all-photos div
+  //     }
+  //   })
+  // })
+}
+
+function getPhotos(){
+  var id = this.event.target.dataset["id"];
+  $.getJSON(`/profiles/${id}/photos`), function(data){
+    debugger
+    for (const key in data){
+      let photoid = data[key]["id"];
+      let photoTitle = data[key]["title"];
+      let photoDescription = data[key]["description"];
+      let photoImage = data[key]["description"];
+      
+      console.log(photoid, photoTitle, photoDescription)
+      let photoDiv = $(".all-photos")
+      let title = document.createElement('h1');
+      title.innerHTML = photoTitle;
+      photoDiv.append(title);
+    }
+  }
+}
