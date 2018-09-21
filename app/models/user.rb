2b9a373314f0efea6ccd28f6e7005ea8143800bf
Kim Gonzales <Kimgo589@gmail.com>
2018-09-21 15:54:15 -0400

    build user serializer and username attr

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,8 @@ class User < ApplicationRecord
   has_many :comments
 
   after_create :create_profile
+
+  attr :username
  
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable, :omniauthable, :omniauth_providers =>[:facebook]
